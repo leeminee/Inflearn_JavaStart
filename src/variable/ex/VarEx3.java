854aca1c28702e0594d253cb3756f94d6a481a21
이민이 <mingramer@gmail.com>
2024-11-20 23:02:05 +0900

    커밋안된것들 다시 커밋

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long first = 10000000000L;
+        System.out.println(first);
+
+        boolean b = true;
+        System.out.println(b);
+
+
+    }
+}
