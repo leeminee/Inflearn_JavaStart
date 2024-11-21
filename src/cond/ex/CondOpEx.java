9b7415dfb554e25aec40550709c5198ed6b4c5ee
이민이 <mingramer@gmail.com>
2024-11-21 15:33:02 +0900

    add 안해서 커밋안된것들

--- src/cond/ex/CondOpEx.java
+++ src/cond/ex/CondOpEx.java
@@ -1,4 +1,12 @@
 package cond.ex;
 
-public class 채ㅜ얘ㅔㄸㅌ {
+public class CondOpEx {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+        System.out.println("max = " + max);
+    }
 }
--- src/cond/ex/ScoreEx.java
+++ src/cond/ex/ScoreEx.java
@@ -1,4 +1,19 @@
 package cond.ex;
 
 public class ScoreEx {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if(score >= 90) {
+            System.out.println("학점은 A");
+        } else if(score >= 80) {
+            System.out.println("학점은 B");
+        } else if(score >= 70) {
+            System.out.println("학점은 C");
+        } else if(score >= 60) {
+            System.out.println("학점은 D");
+        } else {
+            System.out.println("F 야");
+        }
+    }
 }
